41f189987f4dc6b1f53cb3c0c5069c78403459cc
DannyLiehr <dannyliehr@gmail.com>
2023-11-14 15:50:31 -0500

    Added count column.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -12,24 +12,67 @@ Office.onReady((info) => {
   }
 });
 
-// export async function run() {
-//   try {
-//     await Excel.run(async (context) => {
-//       /**
-//        * Insert your Excel code here
-//        */
-//       const range = context.workbook.getSelectedRange();
+/*
 
-//       // Read the range address
-//       range.load("address");
+Bookmark: code plan
+,---.         |         ,---.|              
+|    ,---.,---|,---.    |---'|    ,---.,---.
+|    |   ||   ||---'    |    |    ,---||   |
+`---'`---'`---'`---'    `    `---'`---^`   '
 
-//       // Update the fill color
-//       range.format.fill.color = "yellow";
+1. Create a count column. This is required for all sheets
+2. If they wanted alternating rows, do that.
+3. Split the data by how many up they need.
+4. Detect specific columns and format them accordingly.
 
-//       await context.sync();
-//       console.log(`The range address was ${range.address}.`);
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+*/
+
+
+document.getElementById("generate").addEventListener("click", async () => {
+  try {
+    await Excel.run(async (context) => {
+      // Get the active workbook.
+      let sheet = context.workbook.worksheets.getActiveWorksheet();
+      let range = sheet.getUsedRange();
+      // Load properties from the range
+      range.load("address");
+      // Update the fill color. This is for my own sanity.
+      range.format.fill.color = "powderblue";
+      await context.sync();
+      // console.log(`The range address was ${range.address}.`); // The range address was:Sheet1!A1:O7407
+
+      // Create a proper table from the sheet.
+      let table= sheet.tables.add(range.address, true);
+      table.name= "Data";
+      await context.sync();
+
+      // Grab all data in our sheet, and stick it in our ✨new✨ table.
+      const index= new Array();
+      let rangeVal= table.getDataBodyRange().load("values");
+      await context.sync();
+      let numZeros= String(rangeVal).length; // How many 0s
+      console.log(numZeros)
+      // Add the count + zeros (doesn't seem to keep the 0s tho)
+      rangeVal.values.forEach((_row, i)=>{
+        let n= String("0".repeat(numZeros))+String(i+1);  
+        index.push([n]);
+      });
+      index.unshift(["index"]);
+      table.columns.add(0, index);
+      index.numberFormat = "0".repeat(numZeros);
+      await context.sync();
+
+      sheet.getUsedRange().format.autofitColumns();
+      sheet.getUsedRange().format.autofitRows();
+      sheet.activate();
+
+
+      // SPLIT AT NUMBER
+
+      
+
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})
